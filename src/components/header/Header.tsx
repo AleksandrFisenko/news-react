ce69a47b1955de54ccd16b35dc08fff044f14d33
Aleksandr Fisenko <a.fisenko@tg.dunice.net>
2024-08-12 17:41:09 +0300

    start main page

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,10 @@
+import './header.css'
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img src="./src/assets/ibs.png" alt="Company Logo" className="logo" />
+      
+    </header>
+  )
+}
---
+++ src/pages/main_page/MainPage.tsx
@@ -0,0 +1,11 @@
+import Header from "../../components/header/Header";
+import TaskCard from "../../components/task_card/TaskCard";
+
+export default function MainPage() {
+  return (
+    <>
+      <Header />
+      <TaskCard />
+    </>
+  );
+}
